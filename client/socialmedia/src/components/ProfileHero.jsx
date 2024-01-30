3d23d9fd7fb83c3d4447ef907998f45194544ae2
virgilebouali <virgile.bouali@gmail.com>
2024-01-30 02:09:27 +0100

    register with image ok

--- client/socialmedia/src/components/ProfileHero.jsx
+++ client/socialmedia/src/components/ProfileHero.jsx
@@ -1,158 +1,227 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-
 import axios from "axios";
 import jwt_decode from "jwt-decode";
 import { Button } from "@material-tailwind/react";
 import { FaPencilAlt, FaCamera } from "react-icons/fa";
-import { Textarea } from "@material-tailwind/react";
-import toast from "react-hot-toast";
-
-
 
 const ProfileHero = () => {
   const Navigate = useNavigate();
   const [data, setData] = useState([]);
-  const [selectedFile, setSelectedFile] = useState(null);
+  const [items, setItems] = useState([]);
   const [showTextarea, setShowTextarea] = useState(false);
+  const [selectedFile, setSelectedFile] = useState(null);
 
   const token = localStorage.getItem("token");
-  console.log('token user:', token)
+
   useEffect(() => {
     if (token) {
       const decodedToken = jwt_decode(token);
       const userId = decodedToken.userId;
-     console.log('user:', userId)
       const config = {
         headers: {
           Authorization: `Bearer ${token}`,
         },
       };
 
-      axios.get(`http://localhost:3000/user/${userId}`, config)
+      axios
+        .get(`http://localhost:3000/user/${userId}`, config)
         .then((response) => {
           const data = response.data;
           setData(data);
         })
         .catch((error) => {
-          console.error('Erreur lors de la récupération des favoris :', error);
+          console.error("Erreur lors de la récupération des favoris :", error);
         });
     }
   }, [token]);
 
-    
+  useEffect(() => {
+    const getUserItems = async () => {
+      if (token) {
+        const decodedToken = jwt_decode(token);
+        const userId = decodedToken.userId;
 
-  const handleUpload = async (req) => {
-    // Assurez-vous que req.userId est correctement défini
-  
-    const decodedToken = jwt_decode(token);
-    const serverUserId = decodedToken.userId;
-  
-    const config = {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
+        const config = {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        };
+
+        try {
+          const res = await axios.get(`http://localhost:3000/user/${userId}/items`, config);
+          setItems(res.data.userItems);
+        } catch (error) {
+          console.error(
+            "Erreur lors de la récupération des données de l'utilisateur :",
+            error
+          );
+        }
+      }
     };
-  
-    console.log(serverUserId);
-  
+
+    getUserItems();
+  }, [token]);
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    setSelectedFile(file);
+  };
+
+  const handleUpdateProfilePicture = async () => {
     try {
+      if (!selectedFile) {
+        // Vérifiez si un fichier a été sélectionné
+        console.error("Aucun fichier sélectionné.");
+        return;
+      }
+  
       const formData = new FormData();
-      formData.append('profilePicture', selectedFile);
+      formData.append("profilePicture", selectedFile);
   
-      await axios.patch(
-        `http://localhost:3000/user/${serverUserId}/uploadprofilpicture`,
-        formData,
-        config
-      );
+      const decodedToken = jwt_decode(token);
+      const userId = decodedToken.userId;
   
-      console.log(formData);
-      toast.success('Photo téléchargée avec succès');
+      const config = {
+        headers: {
+          "Content-Type": "multipart/form-data",
+          Authorization: `Bearer ${token}`,
+        },
+      };
   
-      // Mettez à jour l'interface utilisateur ou affichez un message de réussite
+      // Utilisez la bonne URL pour la mise à jour de l'image du profil
+      await axios.patch(`/user/${userId}/uploadprofilpicture`, formData, config);
+      console.log(formData)
+      // Rafraîchir les données de l'utilisateur après la mise à jour
+      const updatedUserData = await axios.get(`http://localhost:3000/user/${userId}`, config);
+      setData(updatedUserData.data);
+  
+      // Réinitialiser le fichier sélectionné
+      setSelectedFile(null);
     } catch (error) {
-      console.error('Erreur lors du téléchargement de l\'image de profil :', error);
-      // Gérez les erreurs et affichez un message d'échec si nécessaire
+      console.error("Erreur lors de la mise à jour de la photo de profil :", error);
     }
   };
   return (
-	<>
-    <div className=" p-8 sm:flex sm:space-x-6 bg-white dark:text-gray-900 my-12 shadow-md w-auto mr-12 border justify-stretch">
-	<div className="flex-shrink-0 w-full mb-6 h-44 sm:h-32 sm:w-32 sm:mb-0">
-		<img src="https://source.unsplash.com/500x500/?portrait?1" alt="" className="object-cover object-center w-full h-full rounded-full dark:bg-gray-500" />
-    <Button variant="filled" className="rounded-full text-black text-sm">
-  <label htmlFor="file-input" className="cursor-pointer">
-    <FaCamera className="text-black hover:text-yellow" />
-  </label>
-  <input
-    id="file-input"
-    type="file"
-    name="profilePicture"
-    encType="multipart/form-data"
-    className="hidden"
-    onChange={handleUpload}
-  />
-</Button>
+    <>
+      <div className="p-8 sm:flex sm:space-x-6 bg-white dark:text-gray-900 my-12 shadow-md w-auto mr-12 border justify-stretch">
+        <div className="flex-shrink-0 w-full mb-6 h-44 sm:h-32 sm:w-32 sm:mb-0">
+          <img
+            src={data.image}
+            alt=""
+            className="object-cover object-center w-full h-full rounded-full dark:bg-gray-500"
+          />
+          <Button variant="filled" className="rounded-full text-black text-sm " onClick={handleUpdateProfilePicture}>
+            <label htmlFor="file-input" className="cursor-pointer">
+              <FaCamera className="text-black hover:text-yellow" />
+            </label>
+            <input
+              id="file-input"
+              type="file"
+              name="profilePicture"
+              encType="multipart/form-data"
+              className="hidden"
+              onChange={handleFileChange}
 
-	</div>
-	<div className="flex flex-col space-y-4">
-		<div>
-			<h2 className="text-2xl font-semibold first-letter:uppercase">{data.username}</h2>
-			<span className=" dark:text-gray-800 text-xl">Points de troc : {data.points}</span>
-		</div>
-		<div className="space-y-1">
-			<span className="flex items-center space-x-2">
-				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" aria-label="Email address" className="w-4 h-4">
-					<path fill="currentColor" d="M274.6,25.623a32.006,32.006,0,0,0-37.2,0L16,183.766V496H496V183.766ZM464,402.693,339.97,322.96,464,226.492ZM256,51.662,454.429,193.4,311.434,304.615,256,268.979l-55.434,35.636L57.571,193.4ZM48,226.492,172.03,322.96,48,402.693ZM464,464H48V440.735L256,307.021,464,440.735Z"></path>
-				</svg>
-				<span className="dark:text-gray-900">{data.email}</span>
-			</span>
-			<span className="flex items-center space-x-2">
-				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" aria-label="Phonenumber" className="w-4 h-4">
-					<path fill="currentColor" d="M449.366,89.648l-.685-.428L362.088,46.559,268.625,171.176l43,57.337a88.529,88.529,0,0,1-83.115,83.114l-57.336-43L46.558,362.088l42.306,85.869.356.725.429.684a25.085,25.085,0,0,0,21.393,11.857h22.344A327.836,327.836,0,0,0,461.222,133.386V111.041A25.084,25.084,0,0,0,449.366,89.648Zm-20.144,43.738c0,163.125-132.712,295.837-295.836,295.837h-18.08L87,371.76l84.18-63.135,46.867,35.149h5.333a120.535,120.535,0,0,0,120.4-120.4v-5.333l-35.149-46.866L371.759,87l57.463,28.311Z"></path>
-				</svg>
-				<span className="dark:text-gray-900">+25 381 77 983</span>
-			</span>
-		</div>
-		<div className="max-w-2xl mx-auto">
-{/* Utilisation de la condition ternaire pour montrer ou cacher le textarea */}
-{showTextarea ? (
-            <textarea
-              id="message"
-              rows="4"
-              className="block p-2.5 text-sm w-96 text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Je suis..."
-            ></textarea>
-          ) : null}
+            />
+          </Button>
+        </div>
+        <div className="flex flex-col space-y-4">
+          <div>
+            <h2 className="text-2xl font-semibold first-letter:uppercase">
+              {data.username}
+            </h2>
+            <span className="dark:text-gray-800 text-xl">
+              Points de troc : {data.points}
+            </span>
+          </div>
+          <div className="space-y-1">
+            <span className="flex items-center space-x-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512"
+                aria-label="Email address"
+                className="w-4 h-4"
+              >
+                <path
+                  fill="currentColor"
+                  d="M274.6,25.623a32.006,32.006,0,0,0-37.2,0L16,183.766V496H496V183.766ZM464,402.693,339.97,322.96,464,226.492ZM256,51.662,454.429,193.4,311.434,304.615,256,268.979l-55.434,35.636L57.571,193.4ZM48,226.492,172.03,322.96,48,402.693ZM464,464H48V440.735L256,307.021,464,440.735Z"
+                ></path>
+              </svg>
+              <span className="dark:text-gray-900">{data.email}</span>
+            </span>
+            <span className="flex items-center space-x-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512"
+                aria-label="Phonenumber"
+                className="w-4 h-4"
+              >
+                <path
+                  fill="currentColor"
+                  d="M449.366,89.648l-.685-.428L362.088,46.559,268.625,171.176l43,57.337a88.529,88.529,0,0,1-83.115,83.114l-57.336-43L46.558,362.088l42.306,85.869.356.725.429.684a25.085,25.085,0,0,0,21.393,11.857h22.344A327.836,327.836,0,0,0,461.222,133.386V111.041A25.084,25.084,0,0,0,449.366,89.648Zm-20.144,43.738c0,163.125-132.712,295.837-295.836,295.837h-18.08L87,371.76l84.18-63.135,46.867,35.149h5.333a120.535,120.535,0,0,0,120.4-120.4v-5.333l-35.149-46.866L371.759,87l57.463,28.311Z"
+                ></path>
+              </svg>
+              <span className="dark:text-gray-900">+25 381 77 983</span>
+            </span>
+          </div>
+          <div className="max-w-2xl mx-auto">
+            {/* Utilisation de la condition ternaire pour montrer ou cacher le textarea */}
+            {showTextarea ? (
+              <textarea
+                id="message"
+                rows="4"
+                className="block p-2.5 text-sm w-96 text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Je suis..."
+              ></textarea>
+            ) : null}
 
-<Button
+            <Button
               variant="filled"
               className="rounded-full text-black text-sm flex mr-4 hover:text-green"
               onClick={() => setShowTextarea(!showTextarea)}
             >
-				Modifier
+              Modifier
               <FaPencilAlt className="text-black ml-4 hover:text-green" />
             </Button>
-	
-    <script src="https://unpkg.com/flowbite@1.4.0/dist/flowbite.js"></script>
-</div>
-	</div>
-	
-</div>
+    
+              </div>
+        </div>
+      </div>
+      <div className="grid gap-5 row-gap-5 mb-8 lg:grid-cols-4 sm:grid-cols-2 h-screen">
+  
+  {items.map((item) => (
+      <a
+        key={item._id} // Assurez-vous d'utiliser une clé unique pour chaque élément
+        href="/"
+        aria-label="View Item"
+        className="inline-block overflow-hidden duration-300 transform bg-white rounded shadow-sm hover:-translate-y-2"
+      >
+        <div className="flex flex-col h-full">
+          <img
+    src={`public/${item.image}`}
 
-<div className="flex">
-	<div className="max-w-xs rounded-md shadow-xl bg-white text-black ">
-	<img src="https://source.unsplash.com/random/300x300/?2" alt="" className="object-cover object-center w-full rounded-t-md h-72 dark:bg-gray-500" />
-	<div className="flex flex-col justify-between p-6 space-y-8">
-		<div className="space-y-2">
-			<h2 className="text-3xl font-semibold tracki text-yellow">Donec lectus leo</h2>
-			<p className="text-black">Curabitur luctus erat nunc, sed ullamcorper erat vestibulum eget.</p>
-		</div>
-		<button type="button" className="flex items-center justify-center w-full p-3 font-semibold tracki rounded-md border-solid border-2 dark:bg-white shadow-2xl dark:text-gray-900">Modifier</button>
-	</div>
-</div>
-	</div>
-</>
+    className="object-cover w-full h-48"
+            alt=""
+          />
+          <div className="flex-grow border border-t-0 rounded-b">
+            <div className="p-5">
+              <h6 className="mb-2 font-semibold leading-5 text-xl ">
+                {item.title} {/* Utilisez la propriété de l'élément pour définir le titre */}
+              </h6>
+              <p className="text-sm text-gray-900 italic">Je veux  : {item.description}</p>
+              <p className="text-sm text-gray-900 italic">En echange de : {item.need}</p>
+              <div className="flex justify-between">
+              <p className="text-lg mt-2 text-yellow font-bold bg">{item.category}</p> {/* Utilisez la propriété de l'élément pour définir la description */}
+
+              </div>{/* Utilisez la propriété de l'élément pour définir la description */}
+            </div>
+          </div>
+        </div>
+      </a>
+    ))}
+  </div>
+    </>
   );
 };
 
--- client/socialmedia/src/components/RegisterHero.jsx
+++ client/socialmedia/src/components/RegisterHero.jsx
@@ -1,83 +1,153 @@
-import React from 'react';
-import axios from 'axios';
-import { useForm, Controller } from 'react-hook-form';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useForm, Controller } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 import { Button } from "@material-tailwind/react";
-
 import { toast } from "react-hot-toast";
+
 const RegisterHero = () => {
-  const { register, handleSubmit, formState: { errors } } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const navigate = useNavigate();
 
-  const onSubmit = async (data) => {
-    console.log("Form data:", data);
-
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+  
     try {
-      const response = await axios.post('http://localhost:3000/register', data);
-
-      if (response.data.success) {
-        toast.success('Inscription réussie')
-        console.log('Inscription réussie:', response.data.user);
-        // Rediriger ou effectuer d'autres actions côté client
-      } else {
-        toast.error("Nom d'utilisateur ou e-mail déjà utilisé")
-        console.error('Erreur d\'inscription:', response.data.error);
-      }
+      const formData = new FormData();
+      formData.append("username", e.target.username.value);
+      formData.append("email", e.target.email.value);
+      formData.append("password", e.target.password.value);
+      formData.append("image", e.target.image.files[0]);
+  
+      const response = await axios.post("http://localhost:3000/register", formData);
+      console.log(response.data);
+      console.log(formData)
+      // Réinitialisation de la prévisualisation de l'image
+      toast.success("Inscription réussie !");
+      navigate("/profile");
     } catch (error) {
-      toast.error("Email ou mot de passe incorrect");
-      console.error('Erreur lors de l\'inscription:', error);
+      console.error("Erreur lors de l'inscription :", error);
+      toast.error("Oups ! Vérifiez le formulaire.");
     }
   };
-
   return (
     <section className="p-4 dark:bg-white-800 flex items-center justify-center mb-12 xl:ml-36">
       <div className="container grid gap-6 mx-auto text-center lg:grid-cols-2 xl:grid-cols-5 -ml-4 xl:-ml-12">
         <div className="w-full px-6 py-16 rounded-md sm:px-12 md:px-16 xl:col-span-2 dark:bg-white-900">
-          <h1 className="text-5xl font-bold dark:text-yellow mb-6 text-left">Inscription</h1>
+          <h1 className="text-5xl font-bold dark:text-yellow mb-6 text-left">
+            Inscription
+          </h1>
           <span className="block font-bold dark:text-black text-2xl text-left mb-12">
-            Rejoignez la plus grande communauté <span className="text-green">éco-responsable</span> gratuitement.
+            Rejoignez la plus grande communauté{" "}
+            <span className="text-green">éco-responsable</span> gratuitement.
           </span>
 
+          <form
+            className="bg-white shadow-md container flex flex-col mx-auto space-y-6 text-black"
+            action="http://localhost:3000/register"
+            method="POST"
+            encType="multipart/form-data"
+            onSubmit={handleFormSubmit}
+          >
+            <input
+              className="bg-white shadow-md rounded px-8 pt-6 pb-8 border border-black"
+              {...register("username", { required: true, max: 12, min: 5 })}
+              type="text"
+              placeholder="Nom d'utilisateur"
+            />
+            <input
+              className="bg-white shadow-md rounded px-8 pt-6 pb-8 border border-black"
+              {...register("email", {
+                required: true,
+                max: 30,
+                min: 5,
+                pattern: /^\S+@\S+$/i,
+              })}
+              type="text"
+              placeholder="Email"
+            />
+            <input
+              className="bg-white shadow-md rounded px-8 pt-6 pb-8 border border-black"
+              {...register("password", { required: true, max: 12, min: 8 })}
+              type="password"
+              placeholder="Mot de Passe"
+            />
+            <div className="mb-3">
+              <label
+                name="image"
+                className="mb-2 inline-block text-black"
+              >
+                Choisissez une photo de profil
+              </label>
+              <input
+  className="relative m-0 block w-full *:h-fit min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-clip-padding px-3 py-[0.32rem] text-base font-normal text-neutral-900 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 file:px-3 file:py-[0.32rem] file:text-neutral-900 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-neutral-200 focus:border-primary focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:border-neutral-600 dark:text-neutral-400 dark:file:bg-neutral-700 dark:file:text-neutral-400 dark:focus:border-primary"
+  type="file"
+  id="formFile"
+  name="image"  // Assurez-vous que le nom du champ est "image"
+  encType="multipart/form-data"
+/>
+            </div>
 
-          <form className="bg-white shadow-md container flex flex-col mx-auto space-y-6" action="http://localhost:3000/register" method="POST "onSubmit={handleSubmit(onSubmit)}>
-  <input className="bg-white shadow-md rounded px-8 pt-6 pb-8  border border-black" type="text" placeholder="Nom d'utilisateur"  {...register("username", { required: true, max: 12, min: 5 })} />
-  <input className="bg-white shadow-md rounded px-8 pt-6 pb-8  border border-black" type="text" placeholder="Email" {...register("email", { required: true, max: 30, min: 5, pattern: /^\S+@\S+$/i })} />
-  <input className="bg-white shadow-md rounded px-8 pt-6 pb-8  border border-black" type="password" placeholder="Mot de Passe" {...register("password", { required: true, max: 12, min: 8 })} />
-  <input className="bg-green shadow-md rounded px-8 pt-6 pb-8 mb-4 cursor-pointer font-bold text-white spacin" type="submit" value="S'inscrire" />
- 
-    <div className="flex flex-col items-center gap-4">
-      <Button
-        size="xl"
-        variant="outlined"
-        color="blue-gray"
-        className="flex items-center gap-3 p-6 shadow-md"
-      >
-        <img src="https://docs.material-tailwind.com/icons/google.svg" alt="metamask" className="h-6 w-6" />
-Se connecter avec Google      </Button>
-      <Button
-        size="lg"
-        variant="gradient"
-        color="light-blue"
-        className="group relative flex items-center gap-3 overflow-hidden pr-[72px]"
-      >
-        Sign in with Twitter
-        <span className="absolute right-0 grid h-full w-12 place-items-center bg-light-blue-600 transition-colors group-hover:bg-light-blue-700">
-          <img src="https://docs.material-tailwind.com/icons/twitter.svg" alt="metamask" className="h-6 w-6" />
-        </span>
-      </Button>
-    </div>
+            <input
+              className="bg-green shadow-md rounded px-8 pt-6 pb-8 mb-4 cursor-pointer font-bold text-white spacing"
+              type="submit"
+              value="S'inscrire"
+            />
 
-</form>
-
-
-
+            <div className="flex flex-col items-center gap-4">
+              <Button
+                size="xl"
+                variant="outlined"
+                color="blue-gray"
+                className="flex items-center gap-3 p-6 shadow-md"
+              >
+                <img
+                  src="https://docs.material-tailwind.com/icons/google.svg"
+                  alt="metamask"
+                  className="h-6 w-6"
+                />
+                Se connecter avec Google
+              </Button>
+              <Button
+                size="lg"
+                variant="gradient"
+                color="light-blue"
+                className="group relative flex items-center gap-3 overflow-hidden pr-[72px]"
+              >
+                Sign in with Twitter
+                <span className="absolute right-0 grid h-full w-12 place-items-center bg-light-blue-600 transition-colors group-hover:bg-light-blue-700">
+                  <img
+                    src="https://docs.material-tailwind.com/icons/twitter.svg"
+                    alt="metamask"
+                    className="h-6 w-6"
+                  />
+                </span>
+              </Button>
+            </div>
+          </form>
         </div>
         {/* Condition pour afficher l'image seulement sur les écrans larges */}
         <div className="hidden xl:flex">
-        <img src="../public/register.jpg" alt="" className="object-cover w-auto h-5/6 rounded-md dark:bg-gray-500 mt-12 ml-12" />
-        <img src="../public/register2.jpg" alt="" className="object-cover w-auto h-5/6 rounded-md dark:bg-gray-500 mt-12 ml-12" />
+          <img
+            src="../public/register.jpg"
+            alt=""
+            className="object-cover w-auto h-5/6 rounded-md dark:bg-gray-500 mt-12 ml-12"
+          />
+          <img
+            src="../public/register2.jpg"
+            alt=""
+            className="object-cover w-auto h-5/6 rounded-md dark:bg-gray-500 mt-12 ml-12"
+          />
 
-        <img src="../public/register3.jpg" alt="" className="object-cover w-auto h-5/6 rounded-md dark:bg-gray-500 mt-12 ml-12" />
-
-
+          <img
+            src="../public/register3.jpg"
+            alt=""
+            className="object-cover w-auto h-5/6 rounded-md dark:bg-gray-500 mt-12 ml-12"
+          />
         </div>
       </div>
     </section>
